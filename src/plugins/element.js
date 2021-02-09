264f9c7982af621bb61bf71108ba5ec34eaf18cf
ifer-itcast <yangkangkang@itcast.cn>
2021-02-09 17:52:45 +0800

    init

---
+++ src/plugins/element.js
@@ -0,0 +1,8 @@
+// import Vue from 'vue'
+
+// import { CheckboxGroup, Checkbox, Card, Button } from 'element-ui'
+
+// Vue.use(CheckboxGroup)
+// Vue.use(Checkbox)
+// Vue.use(Card)
+// Vue.use(Button)
